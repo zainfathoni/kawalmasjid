92f88d71b3b1d288b4c0cbbedb724192f2d8e568
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-20 04:25:21 +0700

    fix: user edit place form

--- app/routes/user.places.$placeId.edit._index.tsx
+++ app/routes/user.places.$placeId.edit._index.tsx
@@ -13,51 +13,49 @@ import {
   Label,
   RemixForm,
   TextArea,
-  TooltipAuto,
 } from "~/components";
-import { requireUserSession, updateNoteSlug } from "~/helpers";
-import { useRootLoaderData } from "~/hooks";
+import { requireUserSession, updatePlaceSlug } from "~/helpers";
 import { model } from "~/models";
-import { schemaNoteUpdate } from "~/schemas";
+import { schemaPlaceUpdate } from "~/schemas";
 import { createSitemap, invariant } from "~/utils";
 
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import type { z } from "zod";
-import { InfoEmpty, SubmitDocument } from "~/icons";
+import { Mosque } from "~/icons";
 
 export const handle = createSitemap();
 
 export async function loader({ request, params }: LoaderArgs) {
   const { userSession } = await requireUserSession(request);
-  invariant(params.noteId, "noteId not found");
+  invariant(params.placeId, "placeId not found");
 
-  const note = await model.userNote.query.getById({
-    id: params.noteId,
+  const place = await model.userPlace.query.getById({
+    id: params.placeId,
     userId: userSession.id,
   });
-  invariant(note, "Note not found");
+  invariant(place, "Place not found");
 
-  const isOwner = userSession.id === note.userId;
+  const isOwner = userSession.id === place.userId;
   if (!isOwner) {
-    return forbidden({ note: null, isOwner: null });
+    return forbidden({ place: null, isOwner: null });
   }
 
-  return json({ note, isOwner });
+  return json({ place, isOwner });
 }
 
 export async function action({ request, params }: ActionArgs) {
   const { userSession } = await requireUserSession(request);
 
   const formData = await request.formData();
-  const submission = parse(formData, { schema: schemaNoteUpdate });
+  const submission = parse(formData, { schema: schemaPlaceUpdate });
   if (!submission.value || submission.intent !== "submit") {
     return badRequest(submission);
   }
 
   try {
-    const newSlug = updateNoteSlug(submission.value);
-    const result = await model.userNote.mutation.update({
-      note: submission.value,
+    const newSlug = updatePlaceSlug(submission.value);
+    const result = await model.userPlace.mutation.update({
+      place: submission.value,
       user: userSession,
     });
     if (!result) {
@@ -71,44 +69,33 @@ export async function action({ request, params }: ActionArgs) {
 }
 
 export default function Route() {
-  const { user } = useRootLoaderData();
-  const { note } = useLoaderData<typeof loader>();
+  const { place } = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
   const navigation = useNavigation();
 
   const isSubmitting = navigation.state === "submitting";
 
-  const [form, { id, slug, title, description, content }] = useForm<
-    z.infer<typeof schemaNoteUpdate>
+  const [form, { id, slug, name, description }] = useForm<
+    z.infer<typeof schemaPlaceUpdate>
   >({
     shouldValidate: "onSubmit",
     lastSubmission: actionData,
     onValidate({ formData }) {
-      return parse(formData, { schema: schemaNoteUpdate });
+      return parse(formData, { schema: schemaPlaceUpdate });
     },
-    constraint: getFieldsetConstraint(schemaNoteUpdate),
+    constraint: getFieldsetConstraint(schemaPlaceUpdate),
   });
 
-  if (!note) {
-    return <span>Note not found.</span>;
+  if (!place) {
+    return <span>Place not found.</span>;
   }
 
   return (
     <div className="stack">
       <header className="py-4">
         <h1 className="queue-center text-3xl">
-          <SubmitDocument />
-          <span>Edit note</span>
-          <TooltipAuto
-            content={
-              <p>
-                A note can be a blog post, news article, <br /> tutorial, or
-                just a regular note
-              </p>
-            }
-          >
-            <InfoEmpty className="size-md" />
-          </TooltipAuto>
+          <Mosque className="size-lg" />
+          <span>Ubah informasi masjid</span>
         </h1>
       </header>
 
@@ -119,50 +106,38 @@ export default function Route() {
         >
           <div className="dim stack text-xs">
             <span>
-              ID: <code>{note.id}</code>
+              ID: <code>{place.id}</code>
             </span>
             <span>
-              Slug: <code>{note.slug}</code>
+              Slug: <code>{place.slug}</code>
             </span>
           </div>
 
-          <input hidden {...conform.input(id)} defaultValue={note.id} />
-          <input hidden {...conform.input(slug)} defaultValue={note.slug} />
+          <input hidden {...conform.input(id)} defaultValue={place.id} />
+          <input hidden {...conform.input(slug)} defaultValue={place.slug} />
 
           <div className="space-y-1">
-            <Label htmlFor={title.id}>Title</Label>
+            <Label htmlFor={name.id}>Nama masjid</Label>
             <Input
-              {...conform.input(title)}
+              {...conform.input(name)}
               type="text"
-              placeholder="Add a title"
-              defaultValue={note.title}
+              placeholder="Nama masjid"
+              defaultValue={place.name}
               className="border-none px-0 sm:text-xl"
             />
-            <Alert id={title.errorId}>{title.error}</Alert>
+            <Alert id={name.errorId}>{name.error}</Alert>
           </div>
 
           <div className="space-y-1">
-            <Label htmlFor={description.id}>Description</Label>
-            <Input
+            <Label htmlFor={description.id}>Deskripsi atau keterangan</Label>
+            <TextArea
               {...conform.input(description)}
-              type="text"
-              placeholder="Add a description"
-              defaultValue={note.description}
-              className="border-none px-0 sm:text-xl"
+              placeholder="Masukkan dekripsi dan keterangan masjid, termasuk kontak, alamat, dll. Maksimum 5000 karakter."
+              rows={10}
+              defaultValue={place.description}
+              className="border-none px-1 sm:text-lg"
             />
             <Alert id={description.errorId}>{description.error}</Alert>
-          </div>
-
-          <div className="space-y-1">
-            <Label htmlFor={content.id}>Content</Label>
-            <TextArea
-              {...conform.input(content)}
-              placeholder="Type your longer content here, maximum content length of 10,000 characters..."
-              rows={10}
-              defaultValue={note.content}
-              className="overflow-y-scroll border-none px-0 sm:text-xl"
-            />
-            <Alert id={content.errorId}>{content.error}</Alert>
           </div>
 
           <div className="queue-center">
@@ -171,20 +146,20 @@ export default function Route() {
               name="intent"
               value="submit"
               isSubmitting={isSubmitting}
-              loadingText="Updating..."
+              loadingText="Mengubah informasi..."
               className="grow"
             >
-              Update
+              Ubah Informasi
             </ButtonLoading>
             <Button type="reset" variant="subtle">
               Reset
             </Button>
             <ButtonLink
-              to={`/${user?.username}/${note.slug}`}
+              to={`/places/${place.slug}`}
               variant="ghost"
               accent="red"
             >
-              Cancel
+              Batal
             </ButtonLink>
           </div>
         </fieldset>
